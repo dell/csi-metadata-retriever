8d6d4583c4e880bc7879cdc9abcb6182397dfb99
Francis Nijay <francis.nijay@dell.com>
2022-05-18 05:51:07 +0530

    Changes env package

--- utils/utils.go
+++ utils/utils.go
@@ -4,15 +4,20 @@ import (
 	"errors"
 	"net"
 	"os"
+	"regexp"
 
 	gocsiutils "github.com/dell/gocsi/utils"
+)
+
+var (
+	emptyRX = regexp.MustCompile(`^\s*$`)
 )
 
 // GetCSIEndpoint returns the network address specified by the
 // environment variable CSI_RETRIEVER_ENDPOINT.
 func GetCSIEndpoint() (network, addr string, err error) {
 	protoAddr := os.Getenv(EnvVarEndpoint)
-	if gocsiutils.emptyRX.MatchString(protoAddr) {
+	if emptyRX.MatchString(protoAddr) {
 		return "", "", errors.New("missing CSI_RETRIEVER_ENDPOINT")
 	}
 	return gocsiutils.ParseProtoAddr(protoAddr)

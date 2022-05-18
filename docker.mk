917131f8a3b55412c87477e228672bd058671d8d
M <Shanmugapriya.M@Dell.com>
2022-05-18 13:18:47 +0530

    docker.mk file updates

--- docker.mk
+++ docker.mk
@@ -2,7 +2,6 @@
 -include vars.mk
 
 # Includes the following generated file to get semantic version information
-include semver.mk
 ifdef NOTES
 	RELNOTE="$(NOTES)"
 else
@@ -28,17 +27,29 @@ else
 	BUILDER=docker
 endif
 
+ifndef MAJOR
+	MAJOR=1
+endif
+
+ifndef MINOR
+	MINOR=0
+endif
+
+ifndef PATCH
+	PATCH=0
+endif
+
 docker:
-	echo "MAJOR $(MAJOR) MINOR $(MINOR) PATCH $(PATCH) RELNOTE $(RELNOTE) SEMVER $(SEMVER)"
+	echo "MAJOR $(MAJOR) MINOR $(MINOR) PATCH $(PATCH) RELNOTE $(RELNOTE)"
 	echo "$(DOCKER_FILE)"
 	$(BUILDER) build -f $(DOCKER_FILE) -t "$(DOCKER_REGISTRY)/$(DOCKER_IMAGE_NAME):v$(MAJOR).$(MINOR).$(PATCH)$(RELNOTE)" --build-arg BASEIMAGE=$(BASEIMAGE) .
 
 docker-no-cache:
-	echo "MAJOR $(MAJOR) MINOR $(MINOR) PATCH $(PATCH) RELNOTE $(RELNOTE) SEMVER $(SEMVER)"
+	echo "MAJOR $(MAJOR) MINOR $(MINOR) PATCH $(PATCH) RELNOTE $(RELNOTE)"
 	echo "$(DOCKER_FILE) --no-cache"
 	$(BUILDER) build --no-cache --pull -f $(DOCKER_FILE) -t "$(DOCKER_REGISTRY)/$(DOCKER_IMAGE_NAME):v$(MAJOR).$(MINOR).$(PATCH)$(RELNOTE)" --build-arg BASEIMAGE=$(BASEIMAGE) .
 
 
 push:   
-	echo "MAJOR $(MAJOR) MINOR $(MINOR) PATCH $(PATCH) RELNOTE $(RELNOTE) SEMVER $(SEMVER)"
+	echo "MAJOR $(MAJOR) MINOR $(MINOR) PATCH $(PATCH) RELNOTE $(RELNOTE)"
 	$(BUILDER) push "$(DOCKER_REGISTRY)/$(DOCKER_IMAGE_NAME):v$(MAJOR).$(MINOR).$(PATCH)$(RELNOTE)"

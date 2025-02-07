68b681ec3f2d9300d6eb7ef5945bcf0c799f868a
Wilson Radadia <wilson.radadia@dell.com>
2025-02-07 15:41:37 +0530

    Update UT coverage for main.go

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -40,16 +42,120 @@ func TestIsExitSignal(t *testing.T) {
 }
 
 func TestTrapSignals(t *testing.T) {
+	// Test case: SIGTERM
+	onExitCalled := false
 	onExit := func() {
-		t.Log("onExit")
+		onExitCalled = true
 	}
-	onAbort := func() {
-		t.Log("onAbort")
-	}
+	onAbort := func() {}
 	trapSignals(onExit, onAbort)
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGTERM)
 	sigc <- syscall.SIGTERM
 	signal.Stop(sigc)
 	close(sigc)
+	if onExitCalled {
+		t.Error("Expected onExit to be called")
+	}
+
+	// Test case: SIGHUP
+	onExitCalled = false
+	onAbortCalled := false
+	trapSignals(onExit, onAbort)
+	sigc = make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGHUP)
+	sigc <- syscall.SIGHUP
+	signal.Stop(sigc)
+	close(sigc)
+	if onExitCalled {
+		t.Error("Expected onExit to be called")
+	}
+	if onAbortCalled {
+		t.Error("Expected onAbort to not be called")
+	}
+
+	// Test case: SIGINT
+	onExitCalled = false
+	onAbortCalled = false
+	trapSignals(onExit, onAbort)
+	sigc = make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT)
+	sigc <- syscall.SIGINT
+	signal.Stop(sigc)
+	close(sigc)
+	if onExitCalled {
+		t.Error("Expected onExit to be called")
+	}
+	if onAbortCalled {
+		t.Error("Expected onAbort to not be called")
+	}
+
+	// Test case: SIGQUIT
+	onExitCalled = false
+	onAbortCalled = false
+	trapSignals(onExit, onAbort)
+	sigc = make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGQUIT)
+	sigc <- syscall.SIGQUIT
+	signal.Stop(sigc)
+	close(sigc)
+	if onExitCalled {
+		t.Error("Expected onExit to be called")
+	}
+	if onAbortCalled {
+		t.Error("Expected onAbort to not be called")
+	}
+
+	// Test case: SIGABRT
+	onExitCalled = false
+	onAbortCalled = false
+	trapSignals(onExit, onAbort)
+	sigc = make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGABRT)
+	sigc <- syscall.SIGABRT
+	signal.Stop(sigc)
+	close(sigc)
+	if onExitCalled {
+		t.Error("Expected onExit to not be called")
+	}
+	if onAbortCalled {
+		t.Error("Expected onAbort to be called")
+	}
+}
+
+type MockPluginProvider struct {
+}
+
+func (m *MockPluginProvider) Serve(ctx context.Context, l net.Listener) error {
+	return nil
+}
+
+func (m *MockPluginProvider) GracefulStop(ctx context.Context) {
+}
+
+func (m *MockPluginProvider) Stop(ctx context.Context) {
+}
+
+func TestRun(t *testing.T) {
+	var appName, appDescription, appUsage string
+	ctx := context.Background()
+	temp := t.TempDir()
+
+	os.Setenv("CSI_RETRIEVER_ENDPOINT", temp+"/metadata")
+	os.Setenv("X_CSI_ENDPOINT_PERMS", "0777")
+	os.Setenv("X_CSI_ENDPOINT_USER", "root")
+	os.Setenv("X_CSI_ENDPOINT_GROUP", "root")
+	os.Setenv("X_CSI_DEBUG", "true")
+	os.Setenv("X_CSI_LOG_LEVEL", "debug")
+	os.Setenv("X_CSI_PLUGIN_INFO", "my-plugin")
+	os.Setenv("X_CSI_REQ_LOGGING", "true")
+	os.Setenv("X_CSI_REP_LOGGING", "true")
+	os.Setenv("X_CSI_REQ_ID_INJECTION", "true")
+	os.Setenv("X_CSI_SPEC_VALIDATION", "true")
+	os.Setenv("X_CSI_SPEC_REQ_VALIDATION", "true")
+	os.Setenv("X_CSI_SPEC_REP_VALIDATION", "true")
+	os.Setenv("X_CSI_SPEC_DISABLE_LEN_CHECK", "true")
+
+	Run(ctx, appName, appDescription, appUsage, &MockPluginProvider{})
+
 }
